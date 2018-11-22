968a429bd82ba709629e0bd84f0aa3579fc739cf
CAIGL <1424689844@qq.com>
2018-11-23 00:22:58 +0800

    date 201811221200

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        hello world!
+        <home-header></home-header>
     </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader /* es6 键名键值相同 HomeHeader == HomeHeader："HomeHeader" */
+  }
 }
 </script>
 
